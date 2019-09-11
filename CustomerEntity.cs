1ac4dd54acef3cedbdd65aa142cc1d6f45ccdd1e
RIteshSolidity <34639678+RIteshSolidity@users.noreply.github.com>
2019-09-11 06:59:41 +0530

    Create CustomerEntity.cs

---
+++ CustomerEntity.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CustomersLibrary
+{
+    public class CustomerEntity
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+
+        public string CustomerLocation { get; set; }
+
+        public int CustomerIncome { get; set; }
+    }
+}
